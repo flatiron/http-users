d40640a5f699fcc8805ff2fe6f023577ec45fc76
cronopio <aristizabal.daniel@gmail.com>
2012-05-15 01:58:57 -0500

    [tests] success test for initial api of users

--- test/users-api-test.js
+++ test/users-api-test.js
@@ -7,35 +7,46 @@
 
 var assert = require('assert'),
     apiEasy = require('api-easy'),
-    helpers = require('../../helpers');
+    base64  = require('flatiron').common.base64,
+    app     = require('./fixtures/app');
     
-var port = 9002;
+var port = 8080;
 
-var suite = apiEasy.describe('http-users/user/api').addBatch(
-  helpers.macros.requireProvisioner(port)
-);
-
-helpers.testApi(suite, port)
+apiEasy.describe('http-users/user/api')
+  .use('localhost', port)
+  .setHeader('Content-Type', 'application/json')
+  .setHeader('Authorization', 'Basic ' + base64.encode('charlie:1234'))
   .discuss('With valid creds')
   .get('/auth')
     .expect(200)
     .expect('should respond with true', function (error, response, body) {
-      var result = JSON.parse(body); 
+      var result = JSON.parse(body);
+      assert.isNull(error);
       assert.isTrue(result.authorized);
     })
-  .undiscuss()
+  .post('/users/devjitsu', { email: 'initial@email.com', password: '1234' })
+    .expect(201)
+    .expect('should respond with the user created', function (err, res, body) {
+      var result = JSON.parse(body);
+      assert.isNull(err);
+      assert.isObject(result);
+      assert.equal('devjitsu', result.username);
+    })
+  .next()
   .get('/users')
     .expect(200)
     .expect('should respond with a list of users', function (err, res, body) {
-      var result = JSON.parse(body); 
+      var result = JSON.parse(body);
+      assert.isNull(err);
       assert.isObject(result);
       assert.isArray(result.users);
-      assert.lengthOf(result.users, 4);
+      assert.lengthOf(result.users, 3);
     })
   .get('/users/devjitsu')
     .expect(200)
     .expect('should respond with the user', function (err, res, body) {
-      var result = JSON.parse(body); 
+      var result = JSON.parse(body);
+      assert.isNull(err);
       assert.isObject(result);
       assert.isObject(result.user);
       assert.equal(result.user.username, 'devjitsu');
@@ -46,7 +57,8 @@ helpers.testApi(suite, port)
   .get('/users/devjitsu')
     .expect(200)
     .expect('should respond with the user', function (err, res, body) {
-      var result = JSON.parse(body); 
+      var result = JSON.parse(body);
+      assert.isNull(err);
       assert.isObject(result);
       assert.isObject(result.user);
       assert.equal(result.user.username, 'devjitsu');
@@ -82,6 +94,7 @@ helpers.testApi(suite, port)
     .expect(200)
     .expect('should respond with not available', function (err, res, body) {
       var result = JSON.parse(body);
+      assert.isNull(err);
       assert.equal(result.available, false);
     })
   .undiscuss()
@@ -90,15 +103,26 @@ helpers.testApi(suite, port)
     .expect(200)
     .expect('should respond with available', function (err, res, body) {
       var result = JSON.parse(body);
+      assert.isNull(err);
       assert.equal(result.available, true);
     })
   .undiscuss()
   .next()
   .discuss('With a valid username')
-  .discuss('but without other parameters (a new reset request)')
-    .post('/users/test-user/forgot')
-    .expect(200)
+    .post('/users/forgot-password', {
+      shake: '0123456789',
+      email: 'forgot-password@test.com',
+      password: '1234'
+    })
+    .expect(201)
+    .expect('should respond with a valid user', function (err, res, body) {
+      var result = JSON.parse(body);
+      assert.isNull(err);
+      assert.isObject(result);
+      assert.equal('forgot-password', result.username);
+    })
   .undiscuss()
+  .next()
   .discuss('a shake parameter, and no password')
     .post('/users/forgot-password/forgot', { shake: '0123456789' })
     .expect(400)
@@ -115,7 +139,11 @@ helpers.testApi(suite, port)
       shake: '0123456789',
       'new-password': 'secretpassword'
     })
-    .expect(200);
-
-    
-suite.export(module);
+    .expect(200)
+  .undiscuss()
+  .next()
+  .discuss('but without other parameters (a new reset request)')
+    .post('/users/forgot-password/forgot')
+    .expect(200)
+  .undiscuss()
+.export(module);
